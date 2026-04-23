49aeb9d9b55c66fe805d7f05fb1c27fb342f0a66
Al Fatah Rizki Ananda <144166775+alfatahrzk@users.noreply.github.com>
2026-04-23 09:28:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from streamlit_cropper import st_cropper
 
 # 1. KONFIGURASI KUNCI API GEMINI
 genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-2.5-flash')
 
 # 2. PENGATURAN LAYOUT
 st.set_page_config(layout="wide", page_title="ChemCompute Pro")
